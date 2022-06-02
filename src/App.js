688b0b3cc3f67afe0dffc0c48a12fab1232f2b98
dianapardo096 <dcpardoa@correo.udistrital.edu.co>
2022-06-02 16:05:53 -0500

    satilos compoenente

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import styledComponents from 'styled-components';
+import styled from 'styled-components';
 import './App.css';
+import{Formulario, H2,GrupoInput, Input,LeyendaError} from'../src/Elementos/Formulario';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareCheck } from '@fortawesome/free-solid-svg-icons'
+import { faUserAstronaut} from '@fortawesome/free-solid-svg-icons'
 
 
 function App() {
   return (
     <main className= 'login-fondo'>
       <Formulario>
+      <FontAwesomeIcon icon={faUserAstronaut}/>
+      <H2>Inicio de sesión</H2>
         <div>
-          <input type='text' placeholder='Usuario'/>
+          <Input type='text' placeholder='Usuario'/>
+          <FontAwesomeIcon icon={faSquareCheck}/>
         </div>
+        <LeyendaError> loren imspup</LeyendaError>
         <div>
           <input type='text' placeholder='Contraseña'/>
         </div>
--- src/Elementos/Formulario.js
+++ src/Elementos/Formulario.js
@@ -1,4 +1,4 @@
-import styledComponents from 'styled-components';
+import styled from 'styled-components';
 
 const colores ={
     falla: "#bb2929",
@@ -6,3 +6,53 @@ const colores ={
     borde: "#FFFF",
 }
 
+const Formulario = styled.form`
+background-color: rgba(44, 44, 60, 0.7);
+padding: 1rem;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+border-radius: 10px;
+height: 45vh;
+width: 35%;
+}
+`;
+
+const H2 = styled.h2`
+color:white;
+}
+`;
+
+const GrupoInput=styled.div`
+position: relative;
+z-index: 90;
+margin:0px:
+}
+`;
+
+const Input=styled.input`
+width: 100%;
+background-color: #fff;
+border-radius: 8px;
+height: 2rem;
+line-height: 2rem;
+padding: 0px 20px 0px 10px;
+transition: 0.5s ease all;
+border: 3px solid transparent;
+
+&:focus {boder: 3px solid ${colores.borde}
+outline:none;
+box-shadow: 3px 0px 30px rgba(163, 163, 163, 0.4);
+}
+}
+`;
+
+const LeyendaError=styled.input`
+
+}
+`;
+
+
+
+export {Formulario,H2,GrupoInput,Input,LeyendaError}
